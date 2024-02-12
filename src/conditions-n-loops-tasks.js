5a8eabc70a1c2f8a6c024d13ad1290ddc2456eaf
Sergey Zlygostev <zlygostev.sergey1983@gmail.com>
2024-02-12 21:53:55 +0400

    feat: 7 tasks pass

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -92,8 +92,19 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  let isosceles = false;
+  if (
+    (a === b && a + b > c) ||
+    (a === c && a + c > b) ||
+    (b === c && b + c > a)
+  ) {
+    isosceles = true;
+  }
+  if (a === 0 || b === 0 || c === 0) {
+    isosceles = false;
+  }
+  return isosceles;
 }
 
 /**
